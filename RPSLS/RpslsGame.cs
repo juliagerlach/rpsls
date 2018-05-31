6d8ef769a45258d9bc13bfc2bbaf050e3a7ce9a3
Julia Gerlach <juliagerlach442@gmail.com>
2018-05-31 16:08:40 -0500

    adding program files

---
+++ RPSLS/RpslsGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class RpslsGame
+    {
+        //member variables
+
+        //constructor
+
+        //member methods
+
+    }
+}
